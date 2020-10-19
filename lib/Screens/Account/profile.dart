98a93a12fff825202e40056a9c3fae0c1134eab1
vinay11r <vinay.bhati@geeksvila.com>
2020-10-19 20:48:00 +0530

    Merge branch 'temp' of https://github.com/hawkersinbox/Hawkers-frontend into temp
    # Conflicts:
    #	pubspec.lock

--- lib/Screens/Account/profile.dart
+++ lib/Screens/Account/profile.dart
@@ -1,15 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:hawkers/SQLite/Database.dart';
+import 'package:hawkers/SQLite/User.dart';
 // import 'file:///D:/git2/Hawkers-frontend/lib/Screens/Account/updateProfile.dart';
 import 'package:hawkers/Widgets/navigationBar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'updateProfile.dart';
 
+
+List<AppUser> _appUserList = new List();
+
 class Profile extends StatefulWidget {
+
+  final String mFirstName;
+  final String mLastName;
+  final String mEmailID;
+  final String mMobileNumber;
+  final String mUserCity;
+  final String mUserState;
+  final String mUserStreetOne;
+  final String mUserPinCode;
+
+  Profile({
+    this.mFirstName,
+    this.mLastName,
+    this.mEmailID,
+    this.mMobileNumber,
+    this.mUserCity,
+    this.mUserState,
+    this.mUserStreetOne,
+    this.mUserPinCode
+  });
+
+
   @override
-  _ProfileState createState() => _ProfileState();
+  _ProfileState createState() => _ProfileState(
+    mFirstName,
+    mLastName,
+    mEmailID,
+    mMobileNumber,
+    mUserCity,
+    mUserState,
+    mUserStreetOne,
+    mUserPinCode
+  );
 }
 
 class _ProfileState extends State<Profile> {
+
+  String mFirstName;
+  String mLastName;
+  String mEmailID;
+  String mAccessToken;
+  String firstAndLastName;
+  String mMobileNumber;
+  String mUserCity;
+  String mUserState;
+  String mUserStreetOne;
+  String mUserPinCode;
+
+
+  String space = " ";
+
+  _ProfileState(
+      String firstName,
+      String lastName,
+      String emailID,
+      String mobileNumber,
+      String userCity,
+      String userState,
+      String userStreetOne,
+      String userPinCode
+      ){
+    this.mFirstName = firstName;
+    this.mLastName = lastName;
+    this.mEmailID = emailID;
+    this.mMobileNumber = mobileNumber;
+    this.mUserCity = userCity;
+    this.mUserState = userState;
+    this.mUserStreetOne = userStreetOne;
+    this.mUserPinCode = userPinCode;
+
+    print("Profile Mobile Number: $mMobileNumber");
+    print("Profile FirtName: $mFirstName");
+    print("Profile LastName: $mLastName");
+    print("Profile EmailID: $mEmailID");
+    print("Profile UserCity: $mUserCity");
+    print("Profile UserState: $mUserState");
+    print("Profile UserStreetOne: $mUserStreetOne");
+    print("Profile UserPinCode: $mUserPinCode");
+
+  }
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,13 +137,16 @@ class _ProfileState extends State<Profile> {
             children: [
               Center(child: Icon(Icons.account_circle,color: Colors.lightGreen,size: 100,)),
               SizedBox(height: 7,),
-              Text('Ravi Teja',
+              Text(
+                '$mFirstName$space$mLastName'
+              ,
                   style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.w500,
                       color: Colors.lightGreen)),
               SizedBox(height: 5,),
-              Text('qwerty@gmail.com',
+              Text(
+              mEmailID,
                   style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.w500,
@@ -62,7 +155,16 @@ class _ProfileState extends State<Profile> {
         Divider(height: 30,thickness: 1,color: Colors.black,),
         InkWell(
           onTap: (){
-            Navigator.push(context,MaterialPageRoute(builder: (context)=>UpdateProfile()));
+            Navigator.push(context,MaterialPageRoute(builder: (context)=>UpdateProfile(
+              mFirstName: mFirstName,
+              mLastName: mLastName,
+              mEmailID: mEmailID,
+              mMobileNumber: mMobileNumber,
+              mUserCity: mUserCity,
+              mUserState: mUserState,
+              mUserStreetOne: mUserStreetOne,
+              mUserPinCode: mUserPinCode,
+            )));
           },
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -92,4 +194,6 @@ class _ProfileState extends State<Profile> {
         break;
     }
   }
+
+
 }
