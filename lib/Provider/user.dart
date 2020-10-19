ccaa9e72fca863e47de879dfc26b5597b24beffc
vinay11r <vinay.bhati@geeksvila.com>
2020-10-19 20:50:02 +0530

    Merge branch 'temp' of https://github.com/hawkersinbox/Hawkers-frontend into temp
    # Conflicts:
    #	pubspec.lock

--- lib/Provider/user.dart
+++ lib/Provider/user.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
+import 'package:hawkers/Provider/AccessToken.dart';
 import 'package:hawkers/Services/api.dart';
 import 'package:hawkers/Utility/userData.dart';
 import 'package:flutter/material.dart';
 import 'package:hawkers/Models/userModel.dart' as UserModel;
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class User with ChangeNotifier {
@@ -11,7 +13,7 @@ class User with ChangeNotifier {
 
   User();
 
-  Future<bool> tryAutoLogin() async {
+  Future<bool> tryAutoLogin(BuildContext context) async {
     final prefs = await SharedPreferences.getInstance();
     if (!prefs.containsKey('userData')) {
       return false;
@@ -19,6 +21,8 @@ class User with ChangeNotifier {
     try {
       UserData.user =
           UserModel.User.fromJson(json.decode(prefs.getString('userData')));
+      var accessTokenProvider = Provider.of<AccessTokenProvider>(context, listen: false);
+      accessTokenProvider.setAccessToken(UserData.user.response.accessToken.toString());
       notifyListeners();
     } catch (e) {
       print(e);
