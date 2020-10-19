51795982aeee256522582c4c8d8fe62a92595ac7
vinay11r <vinay.bhati@geeksvila.com>
2020-10-19 20:49:25 +0530

    Merge branch 'temp' of https://github.com/hawkersinbox/Hawkers-frontend into temp
    # Conflicts:
    #	pubspec.lock

--- lib/Screens/splashScreen.dart
+++ lib/Screens/splashScreen.dart
@@ -3,6 +3,13 @@ import 'package:hawkers/Provider/user.dart';
 import 'package:hawkers/Screens/HomeScreen.dart';
 import 'package:hawkers/Screens/loginScreen.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+String mFirstName;
+String mLastName;
+String mEmailID;
+String mAccessToken;
 
 class SplashScreen extends StatefulWidget {
   static const routeName = '/SplashScreen';
@@ -14,17 +21,33 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     nextScreen();
+    Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+    _prefs.then((SharedPreferences sharedPreferences) {
+      mFirstName = sharedPreferences.getString("user_first_name");
+      mLastName = sharedPreferences.getString("user_last_name");
+      mEmailID = sharedPreferences.getString("user_email");
+      mAccessToken = sharedPreferences.getString("user_access_token");
+    })
+        .whenComplete(() {
+      print("Complete!");
+    })
+        .catchError((error){
+      print("Error: ${error.toString()}");
+    });
     super.initState();
   }
 
   nextScreen() async {
-    if (await Provider.of<User>(context, listen: false).tryAutoLogin()) {
+    if (await Provider.of<User>(context, listen: false).tryAutoLogin(context)) {
       Future.delayed(Duration.zero, () {
-        Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+        // Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => HomeScreen()));
       });
     } else {
       Future.delayed(Duration.zero, () {
         Navigator.pushReplacementNamed(context, Login.routeName);
+        // Navigator.pushReplacementNamed(context, '/HomeScreen');
       });
     }
   }
