a02818aa64325d4c92b211258a306190eeb4d686
vinay11r <vinay.bhati@geeksvila.com>
2020-10-19 20:45:00 +0530

    Merge branch 'temp' of https://github.com/hawkersinbox/Hawkers-frontend into temp
    # Conflicts:
    #	pubspec.lock

--- lib/Screens/addUserAdmin.dart
+++ lib/Screens/addUserAdmin.dart
@@ -1,5 +1,23 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:hawkers/DataClass/AddUserData.dart';
+import 'package:hawkers/Services/api.dart';
 import 'package:hawkers/Widgets/navigationBar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+RestApi _restApi = RestApi();
+TextEditingController _firstNameController = new TextEditingController();
+TextEditingController _lastNameController = new TextEditingController();
+TextEditingController _mobileNoController = new TextEditingController();
+TextEditingController _emailController = new TextEditingController();
+TextEditingController _streeNameController = new TextEditingController();
+TextEditingController _stateController = new TextEditingController();
+TextEditingController _cityController = new TextEditingController();
+TextEditingController _pinCodeController = new TextEditingController();
+TextEditingController _roleController = new TextEditingController();
+
+
 
 class AddUserAdmin extends StatefulWidget {
   @override
@@ -29,33 +47,33 @@ class _AddUserAdminState extends State<AddUserAdmin> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Field("First Name*",),
+                Field("First Name*", _firstNameController),
                 SizedBox(
                   height: 20,
                 ),
-                Field('Last Name*', ),
+                Field('Last Name*', _lastNameController),
                 SizedBox(height: 20),
-                NumField('Mobile Number*', ),
+                NumField('Mobile Number*', _mobileNoController),
                 SizedBox(
                   height: 20,
                 ),
-                Field('Email Address', ),
+                Field('Email Address', _emailController),
                 SizedBox(
                   height: 20,
                 ),
-                Field('Street Address',),
+                Field('Street Address', _streeNameController),
                 SizedBox(
                   height: 20,
                 ),
-                Field('City*', ),
+                Field('City*', _cityController),
                 SizedBox(
                   height: 20,
                 ),
-                Field('State*', ),
+                Field('State*', _stateController),
                 SizedBox(
                   height: 20,
                 ),
-                NumField('Pincode*',),
+                NumField('Pincode*', _pinCodeController),
                 SizedBox(
                   height: 20,
                 ),
@@ -90,12 +108,12 @@ class _AddUserAdminState extends State<AddUserAdmin> {
                         },
                         items: [
                           DropdownMenuItem(
-                            child: Text("Sales"),
-                            value: 1,
+                            child: Text("SELLER"),
+                            value: 0,
                           ),
                           DropdownMenuItem(
-                            child: Text("Marketing"),
-                            value: 2,
+                            child: Text("MARKETING"),
+                            value: 1,
                           ),
                         //  DropdownMenuItem(child: Text("Noida"), value: 3),
                         ]
@@ -112,8 +130,19 @@ class _AddUserAdminState extends State<AddUserAdmin> {
                       // width:ScreenUtil().setWidth(700),
                       child: RaisedButton(
                         onPressed: () {
-
-
+                          String userRole = getRole(_value);
+                          print("UserRole: ${userRole.toString()}");
+                          addUser(
+                              firstName: _firstNameController.text,
+                              lastName: _lastNameController.text,
+                              mobileNo: _mobileNoController.text,
+                              emailID : _emailController.text,
+                              streetAddressOne : _streeNameController.text,
+                              city : _cityController.text,
+                              state: _stateController.text,
+                              pinCode : _pinCodeController.text,
+                              role: userRole
+                          );
                         },
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular((3))),
@@ -140,7 +169,7 @@ class _AddUserAdminState extends State<AddUserAdmin> {
   }
   Widget NumField(
       String name,
-    //  TextEditingController controller,
+     TextEditingController controller,
       ) {
     return Container(
         child: Column(
@@ -166,7 +195,7 @@ class _AddUserAdminState extends State<AddUserAdmin> {
                   padding: const EdgeInsets.all(4.0),
                   child: TextField(
                     keyboardType: TextInputType.number,
-                 //   controller: controller,
+                   controller: controller,
                     textAlign: TextAlign.start,
                     cursorColor: Colors.black,
                     style: TextStyle(color: Colors.black, fontSize: 21),
@@ -183,7 +212,7 @@ class _AddUserAdminState extends State<AddUserAdmin> {
   }
   Widget Field(
       String name,
-  //    TextEditingController controller,
+     TextEditingController _controller,
       ) {
     return Container(
       child: Column(
@@ -207,14 +236,15 @@ class _AddUserAdminState extends State<AddUserAdmin> {
                 borderRadius: BorderRadius.circular(3)),
             child: Padding(
               padding: const EdgeInsets.only(left: 4.0),
-              child: TextField(
+              child: TextFormField(
                 textCapitalization: TextCapitalization.sentences,
-            //    controller: controller,
+               controller: _controller,
                 textAlign: TextAlign.start,
                 cursorColor: Colors.black,
                 style: TextStyle(color: Colors.black, fontSize: 21),
-                //     color:Colors.grey,
-
+                onChanged: (newValue){
+                  print("NewValue: ${newValue.toString()}");
+                },
                 decoration: InputDecoration(
                   border: InputBorder.none,
                   floatingLabelBehavior: FloatingLabelBehavior.never,
@@ -225,5 +255,75 @@ class _AddUserAdminState extends State<AddUserAdmin> {
         ],
       ),
     );
+  }
+
+  void addUser(
+  {
+    String firstName,
+    String lastName,
+    String mobileNo,
+    String emailID,
+    String streetAddressOne,
+    String city,
+    String state,
+    String pinCode,
+    String role
+}
+      ) {
+
+    final body = jsonEncode({
+      "first_name": firstName,
+      "last_name": lastName,
+      "mobile": mobileNo,
+      "email": emailID,
+      "street_address1": streetAddressOne,
+      "city": city,
+      "state": state,
+      "pincode": pinCode,
+      "role": role
+    });
+
+    Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+    _prefs.then((SharedPreferences sharedPreferences) {
+      String accessToken = sharedPreferences.getString("access_token");
+      print("access_token: $accessToken");
+      var response = _restApi.addUser(accessToken, body);
+      response.then((value)  {
+        String response = value.body.toString();
+        print("Response String: $response}");
+        Map<String, dynamic> _map = json.decode(response) as Map;
+        print("Map String: ${_map.toString()}");
+        var variable = new AddUseData.fromJson(_map);
+        String firstName = variable.response.firstName;
+        print(variable.response.toString());
+        print("FirstName: $firstName");
+      })
+      .catchError((error){
+        print("Error: ${error.toString()}");
+      })
+      .whenComplete(() {
+        print("Complete!");
+      });
+      print("add user response: ${response.toString()}");
+
+    })
+        .whenComplete(() {
+      print("Complete!");
+    })
+        .catchError((error){
+      print("Error: ${error.toString()}");
+    });
+  }
+
+  String getRole(int value) {
+    switch (_value){
+      case 0:
+        return "Sales";
+        break;
+      case 1:
+        return "Marketing";
+        break;
+    }
   }
 }
